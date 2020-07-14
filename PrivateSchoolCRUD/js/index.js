fb21f4cb95b20c0860f6d99634c8d32a10d37aff
Alex K <alexkambos@gmail.com>
2020-07-14 15:54:20 +0300

    Add files via upload
    Implementation of user's interface, on private school CRUD project, without business logic, or data base. Using only HTML, CSS, JS.

---
+++ PrivateSchoolCRUD/js/index.js
@@ -0,0 +1,9 @@
+$(function(){
+    $('form').submit(function(){
+        alert("Your entry has been completed!");
+    })
+
+    $('button').submit(function(){
+        alert("Your entry has been completed!");
+    })
+})
